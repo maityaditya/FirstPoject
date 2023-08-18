94487271c9789486cedc9145afab61c59b3e0bd8
maityaditya <maityaditya2325@gmail.com>
2023-08-18 21:36:09 +0530

    New js and css added

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 //This is a elephant
 //This is a tiger
+let url="https://passwordinator.onrender.com/?num=true";
+let password=document.querySelector("#inputPassword");
+let btn=document.querySelector("#button1");
+async function getPass(){
+  try{
+    let res=await axios.get(url);
+    return res.data;
+  }catch(err){
+    console.log("Error found is",err);
+  }
+}
+
+btn.addEventListener("click",async()=>{
+let data = await getPass();
+password.value=data.data;
+});
